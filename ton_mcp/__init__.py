91476fb9082c1e145cd87540925d206f10eb3d04
ness <shonness98@gmail.com>
2026-03-20 23:36:57 +0500

    Update package metadata

--- ton_mcp/__init__.py
+++ ton_mcp/__init__.py
@@ -1,14 +1,14 @@
-from __future__ import annotations
-
 # Copyright (c) 2026 Shon Ness
 #
 # This source code is licensed under the MIT License found in the
 # LICENSE file in the root directory of this source tree.
 
 __all__ = [
+    "__author__",
     "__uri__",
     "__version__",
 ]
 
+__author__ = "nessshon"
 __version__ = "0.0.1"
 __uri__ = "https://github.com/nessshon/ton-mcp"
